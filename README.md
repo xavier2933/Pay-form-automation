1e168337c5c328e0c813701432ac5df60c4c1904
xavier2933 <102980192+xavier2933@users.noreply.github.com>
2023-06-15 19:08:41 -0600

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Pay form automation
+
+This code will fill out the timesheet with data from .csv files. It is very important for these to be saved as .csv files or else this will not work.
+- Make sure files are saved in the correct format, with that being date, time in, time out, and total worked as each of the collumns, respectively. 
+- Do not add any labels or anything, just data. You can use my sheet as an example. Make sure to fill in blank spaces with zeros
+- You will have to edit a couple of things in the init class to match your personal
+information. You can either copy these exactly from the form or from the .html file included. It is important for these to be exact or it won't work. 
+- Totals will be automatically calculated, make sure these are correct before submitting
+ - You will
+(probably) need to install selenium, which can be done with 
+`pip install selenium ` or whatever the equivalent for mac is. 
+- When you run the program you will be redirected to the university's sso page, you will have ~10 seconds to fill this out, and after
+that all you need to do is watch, check that everything is correct, and press submit. The window will automatically close WITHOUT SUBMITTING after 100 seconds
+
---
+++ formAutomation.py
@@ -0,0 +1,103 @@
+#################################################################
+# formAutomation.py
+# Author: Xavier O'Keefe
+# 
+# This uses Selenium to automate my weekly time sheet for my lab.
+#################################################################
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import time
+import csv
+import secretFile
+
+class readData:
+    # Set initial params for data that is not easily read from csv file
+    def __init__(self, inFile):
+        self.days = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"]
+        self.browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.totalHoursWorkedWeek1 = 0
+        self.totalHoursWorkedWeek2 = 0
+        self.bigTotalWorked = 0
+##################################################################################################
+#                        STUFF YOU NEED TO CHANGE
+##################################################################################################
+        self.docToRead = inFile
+        self.name = "Xavier O'Keefe"
+        self.email = "xaok7569@colorado.edu"
+        self.sup_Name = secretFile.superName
+        self.sup_Email = secretFile.superEmail
+        self.url = secretFile.link
+        self.endDate = "6/3"
+
+    # Load the browser
+    def getBrowser(self):
+        self.browser.get(self.url)
+        time.sleep(15)
+
+    # Function to read the CSV file and add data into the form
+    def addData(self):
+        self.getBrowser()
+        name = Select(self.browser.find_element(By.NAME, "realname"))
+        name.select_by_visible_text(self.name)
+        email = self.browser.find_element(By.NAME, "email")
+        email.send_keys(self.email)
+        supName = Select(self.browser.find_element(By.NAME, "Supervisor"))
+        supName.select_by_visible_text(self.sup_Name)
+        supEmail = Select(self.browser.find_element(By.NAME, "recipient"))
+        supEmail.select_by_visible_text(self.sup_Email)
+        endDate = self.browser.find_element(By.NAME, "Pay_Period_End_Date")
+        endDate.send_keys(str(self.endDate))
+
+        i = 0
+        j = 0
+        with open(self.docToRead) as csvFile:
+            csvReader = csv.reader(csvFile, delimiter=',')
+            for row in csvReader:
+                if(i < 7):
+                    (date, inner, out, total) = row
+                    date1 = self.browser.find_element(By.NAME, "Date_" + self.days[i] + "_Week1")
+                    date1.send_keys(str(date))
+                    in1 = self.browser.find_element(By.NAME, "In_" + self.days[i] + "_Week1")
+                    in1.send_keys(str(inner))
+                    out1 = self.browser.find_element(By.NAME, "Out_" + self.days[i] + "_Week1")
+                    out1.send_keys(str(out))
+                    total1 = self.browser.find_element(By.NAME, "Total_Hours_" + self.days[i] + "_Week1")
+                    self.totalHoursWorkedWeek1 += float(total)
+                    total1.send_keys(str(total))
+                    i += 1
+                else:
+                    (date, inner, out, total) = row
+                    date2 = self.browser.find_element(By.NAME, "Date_" + self.days[j] + "_Week2")
+                    date2.send_keys(str(date))
+                    in2 = self.browser.find_element(By.NAME, "In_" + self.days[j] + "_Week2")
+                    in2.send_keys(str(inner))
+                    out2 = self.browser.find_element(By.NAME, "Out_" + self.days[j] + "_Week2")
+                    out2.send_keys(str(out))
+                    total2 = self.browser.find_element(By.NAME, "Total_Hours_" + self.days[j] + "_Week2")
+                    self.totalHoursWorkedWeek2 += float(total)
+                    total2.send_keys(str(total))
+                    j +=1     
+
+        hours1 = self.browser.find_element(By.NAME, "Total_Hours_Week1")
+        hours1.send_keys(str(self.totalHoursWorkedWeek1))  
+        hours2 = self.browser.find_element(By.NAME, "Total_Hours_Week2")
+        hours2.send_keys(str(self.totalHoursWorkedWeek2)) 
+        bigTotal = self.browser.find_element(By.NAME, "Total_Hours_for_Pay_Period")
+        self.bigTotalWorked = self.totalHoursWorkedWeek1 + self.totalHoursWorkedWeek2
+        bigTotal.send_keys(str(self.bigTotalWorked))  
+        time.sleep(100)
+
+    # Quits the browser
+    def quitBrowser(self):
+        self.browser.quit()
+
+if __name__ == "__main__":
+    print()
+    file = input("Which file??  >> ")
+    read = readData(file)
+    read.addData()
+    read.quitBrowser()
+
